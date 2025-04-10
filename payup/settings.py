6d19a1f42ff4dd02d4753e90d09c006f4af3a203
Eaby Thomas C <eabythomascu@gmail.com>
2025-04-10 18:39:20 +0530

    env fix

--- payup/settings.py
+++ payup/settings.py
@@ -1,9 +1,13 @@
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
